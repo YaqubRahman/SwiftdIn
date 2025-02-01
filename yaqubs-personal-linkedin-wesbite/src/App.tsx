4050ca92fd48e91bcafe35d64aa02615bc46d95c
YaqubRahman <YaqubRahman999@gmail.com>
2025-02-01 21:43:19 +0000

    feat:frontend:gui for sign up and login

--- yaqubs-personal-linkedin-wesbite/src/App.tsx
+++ yaqubs-personal-linkedin-wesbite/src/App.tsx
@@ -2,15 +2,18 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home';
+import { LoginSignup } from './LoginSignup/LoginSignup';
 
 export default function App() {
   return (
+    <>
     <Router>
-    <Routes>
-      <Route path="/" element={<Home />}/>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<LoginSignup/>} />
 
-    </Routes>
-    </Router>
+      </Routes>
+    </Router></>
   )
 }
 
---
+++ yaqubs-personal-linkedin-wesbite/src/LoginSignup/LoginSignup.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import './LoginSignup.css'
+
+
+import user_icon from '../assets/LoginSignupAssets/user.png' 
+import email_icon from '../assets/LoginSignupAssets/envelope.png' 
+import password_icon from '../assets/LoginSignupAssets/password.png' 
+
+
+
+export const LoginSignup = () => {
+
+  const [action,setAction] = useState("Sign Up");
+
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>Sign Up</div>
+        <div className='underline'></div>
+      </div>
+      <div className='inputs'>
+
+      <div className='input'>
+        <img src={user_icon} alt=''/>
+        <input type='text' placeholder='Name'/>
+      </div>
+
+      <div className='input'>
+        <img src={email_icon} alt=''/>
+        <input type='email'placeholder='Email ID'/>
+      </div>
+
+      <div className='input'>
+        <img src={password_icon} alt=''/>
+        <input type='password'placeholder='Password'/>
+      </div>
+      </div>
+      <div className="forgot-password">Lost Password? <span>Click Here</span></div>
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit">Login</div>
+      </div>
+
+
+    </div>
+  )
+}
