81c513bc73166d9b371b5b40b0ff7b13f5b8680c
YaqubRahman <YaqubRahman999@gmail.com>
2025-02-01 22:04:07 +0000

    Change:frontend:removed lost password for register and removed name for login

--- yaqubs-personal-linkedin-wesbite/src/LoginSignup/LoginSignup.tsx
+++ yaqubs-personal-linkedin-wesbite/src/LoginSignup/LoginSignup.tsx
@@ -10,20 +10,22 @@ import password_icon from '../assets/LoginSignupAssets/password.png'
 
 export const LoginSignup = () => {
 
-  const [action,setAction] = useState("Sign Up");
+  const [action,setAction] = useState("Login");
 
   return (
     <div className='container'>
       <div className='header'>
-        <div className='text'>Sign Up</div>
+        <div className='text'>{action}</div>
         <div className='underline'></div>
       </div>
       <div className='inputs'>
-
-      <div className='input'>
+        {action==='Login'?<div></div>:
+        <div className='input'>
         <img src={user_icon} alt=''/>
         <input type='text' placeholder='Name'/>
-      </div>
+      </div>}
+
+  
 
       <div className='input'>
         <img src={email_icon} alt=''/>
@@ -35,10 +37,14 @@ export const LoginSignup = () => {
         <input type='password'placeholder='Password'/>
       </div>
       </div>
-      <div className="forgot-password">Lost Password? <span>Click Here</span></div>
+
+      {action==='Sign Up'?<div></div>:
+        <div className="forgot-password">Lost Password? <span>Click Here</span></div>}
+
+
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action=='Login'? 'submit gray': 'submit'} onClick={()=>{setAction('Sign Up')}}>Sign Up</div>
+        <div className={action=='Sign Up'?'submit gray': 'submit'}onClick={()=>{setAction('Login')}}>Login</div>
       </div>
 
 
