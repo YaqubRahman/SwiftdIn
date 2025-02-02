9e278d20270b24c9a9b3f7511b346afd1072146a
YaqubRahman <YaqubRahman999@gmail.com>
2025-02-02 20:12:38 +0000

    Add: handleInputChange function in LoginSignup.tsx

--- yaqubs-personal-linkedin-wesbite/src/pages/LoginSignup.tsx
+++ yaqubs-personal-linkedin-wesbite/src/pages/LoginSignup.tsx
@@ -1,3 +1,5 @@
+import { useForm } from 'react-hook-form'
+
 import React, { useState } from 'react'
 import '../assets/LoginSignupAssets/LoginSignup.css'
 
@@ -12,39 +14,110 @@ export const LoginSignup = () => {
 
   const [action,setAction] = useState("Login");
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+
+    console.log('Form data updated:', formData);
+  };
+
+
+  const handleSubmit = async () => {
+    if (action == 'Login'){
+      try{
+        console.log('Attempting to login with', {email:formData.email, password:formData.password});
+
+        // LOGIN API CALL
+
+
+      } catch (error) {
+        console.error('Login error', error);
+      }
+    } else {
+      try{
+        console.log('Attempting to sign up with', formData);
+
+        // SIGNUP API CALL
+
+
+
+      } catch (error){
+        console.error('Sign Up error', error);
+      }
+    }
+  };
+
+
+
   return (
     <div className='container'>
       <div className='header'>
         <div className='text'>{action}</div>
         <div className='underline'></div>
       </div>
+
+
       <div className='inputs'>
         {action==='Login'?<div></div>:
         <div className='input'>
         <img src={user_icon} alt=''/>
-        <input type='text' placeholder='Name'/>
+        <input
+
+         type='text'
+         name='name'
+         placeholder='Name'/>
+         value={formData.name}
+         onChange={handleInputChange}
+
+
       </div>}
 
   
 
       <div className='input'>
         <img src={email_icon} alt=''/>
-        <input type='email'placeholder='Email ID'/>
+        <input
+
+        type='email'
+        name='email'
+        placeholder='Email ID'
+        value={formData.email}
+        onChange={handleInputChange}
+        
+        />
       </div>
 
       <div className='input'>
         <img src={password_icon} alt=''/>
-        <input type='password'placeholder='Password'/>
+        <input
+
+         type='password'
+         name='password'
+         placeholder='Password'
+         value={formData.password}
+         onChange={handleInputChange}
+        
+        />
       </div>
       </div>
 
       {action==='Sign Up'?<div></div>:
-        <div className="forgot-password">Lost Password? <span>Click Here</span></div>}
+        <div className="forgot-password">Forgot Password? <span>Click Here</span></div>}
 
 
       <div className="submit-container">
-        <div className={action=='Login'? 'submit gray': 'submit'} onClick={()=>{setAction('Sign Up')}}>Sign Up</div>
-        <div className={action=='Sign Up'?'submit gray': 'submit'}onClick={()=>{setAction('Login')}}>Login</div>
+        <div className={action=='Login'? 'submit gray': 'submit'} onClick={()=>{setAction('Sign Up')}}>Sign Up</div> {/*When you click sign up */}
+        <div className={action=='Sign Up'?'submit gray': 'submit'}onClick={()=>{setAction('Login')}}>Login</div>     {/*When you click login */}
       </div>
 
 
