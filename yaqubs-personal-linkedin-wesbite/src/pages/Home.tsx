cc1e9794c4712634979520b2b8f4b4a4c19f1076
YaqubRahman <YaqubRahman999@gmail.com>
2025-03-04 23:54:28 +0000

    feat/connections-box-added-with-static-names

--- yaqubs-personal-linkedin-wesbite/src/pages/Home.tsx
+++ yaqubs-personal-linkedin-wesbite/src/pages/Home.tsx
@@ -1,120 +1,124 @@
-import pfp from '../assets/pfpplaceholder.png'
-import flag from '../assets/UKflag.png'
-import '../components/Home.css'
+import pfp from "../assets/pfpplaceholder.png";
+import flag from "../assets/UKflag.png";
+import "../components/Home.css";
 
-import BangladeshFlag from '../assets/CountryChoose/Bangladesh.png';
-import LebanonFlag from '../assets/CountryChoose/Lebanon.png';
-import TurkishFlag from '../assets/CountryChoose/Turkey.png';
-import SaudiFlag from '../assets/CountryChoose/Saudi.png';
-import IraqFlag from '../assets/CountryChoose/Iraq.png';
-import PakistanFlag from '../assets/CountryChoose/Pakistan.png';
-import MalasiaFlag from '../assets/CountryChoose/Malasia.png';
-import EgyptFlag from '../assets/CountryChoose/Egypt.png';
+import BangladeshFlag from "../assets/CountryChoose/Bangladesh.png";
+import LebanonFlag from "../assets/CountryChoose/Lebanon.png";
+import TurkishFlag from "../assets/CountryChoose/Turkey.png";
+import SaudiFlag from "../assets/CountryChoose/Saudi.png";
+import IraqFlag from "../assets/CountryChoose/Iraq.png";
+import PakistanFlag from "../assets/CountryChoose/Pakistan.png";
+import MalasiaFlag from "../assets/CountryChoose/Malasia.png";
+import EgyptFlag from "../assets/CountryChoose/Egypt.png";
 
 const countries = [
-  { code: 'BD', name: 'Bangladesh',flag: BangladeshFlag},
-  { code: 'LB', name: 'Lebanon', flag: LebanonFlag },
-  { code: 'TR', name: 'Turkey', flag: TurkishFlag },
-  { code: 'SA', name: 'Saudi Arabia', flag: SaudiFlag },
-  { code: 'IQ', name: 'Iraq', flag: IraqFlag },
-  { code: 'PK', name: 'Pakistan', flag: PakistanFlag },
-  { code: 'MY', name: 'Malaysia', flag: MalasiaFlag },
-  { code: 'EG', name: 'Egypt', flag: EgyptFlag }
+  { code: "BD", name: "Bangladesh", flag: BangladeshFlag },
+  { code: "LB", name: "Lebanon", flag: LebanonFlag },
+  { code: "TR", name: "Turkey", flag: TurkishFlag },
+  { code: "SA", name: "Saudi Arabia", flag: SaudiFlag },
+  { code: "IQ", name: "Iraq", flag: IraqFlag },
+  { code: "PK", name: "Pakistan", flag: PakistanFlag },
+  { code: "MY", name: "Malaysia", flag: MalasiaFlag },
+  { code: "EG", name: "Egypt", flag: EgyptFlag },
 ];
 
+const country = localStorage.getItem("country");
 
-const country = localStorage.getItem('country');
-
-const userString = localStorage.getItem('user');
+const userString = localStorage.getItem("user");
 const userData = userString ? JSON.parse(userString) : null;
-const user_name = userData ? userData.firstName : '';
+const user_name = userData ? userData.firstName : "";
 
-function getCountryCode(){
-  console.log('Country from localStorage:', country);
-  for (let i=0; i < countries.length; i++){
-    if (countries[i].code == country){
-      console.log('Matched country:', countries[i].flag);
-      return countries[i].flag
+function getCountryCode() {
+  console.log("Country from localStorage:", country);
+  for (let i = 0; i < countries.length; i++) {
+    if (countries[i].code == country) {
+      console.log("Matched country:", countries[i].flag);
+      return countries[i].flag;
     }
   }
-  console.log('No match found');
+  console.log("No match found");
   return flag;
 }
 
-function getCountryName(){
-  for(let i=0;i<countries.length; i++){
-    if(countries[i].code == country){
-      console.log('Country Name:', countries[i].name );
-      return countries[i].name;      
+function getCountryName() {
+  for (let i = 0; i < countries.length; i++) {
+    if (countries[i].code == country) {
+      console.log("Country Name:", countries[i].name);
+      return countries[i].name;
     }
   }
 }
 
-
 function Home() {
-    return (
-        <>
-        <div className='homefont'>
+  return (
+    <>
+      <div className="homefont">
         <h1>SwiftdIn</h1>
-<div className="parent-container">
-  <div className="containerhome">
-    <div className="childcontainer1">    
-    <div className="namecard">
+        <div className="parent-container">
+          <div className="containerhome">
+            <div className="childcontainer1">
+              <div className="namecard">
+                <img src={pfp} className="logo" alt="pfp PlaceHolder" />
+                <div className="homefont">
+                  <h1>{user_name}</h1>
+                </div>
 
-        <img src={pfp} className="logo" alt="pfp PlaceHolder" />
-      <div className='homefont'>
-      <h1>{user_name}</h1>
+                <p>Junior Software Engineer</p>
 
-      </div>
-      
-      <p>Junior Software Engineer</p>
+                <p>Currently working at SwiftdIn as a full stack engineer</p>
 
-      <p>Currently working at SwiftdIn as a full stack engineer</p>
+                <img
+                  src={getCountryCode()}
+                  className="flag"
+                  alt="Country Flag"
+                />
 
-        <img src={getCountryCode()} className="flag" alt="Country Flag" />
+                <p>{getCountryName()}</p>
+              </div>
+
+              <div className="card">
+                <h2>Experience</h2>
+
+                <div className="trending">
+                  <p>
+                    2018-Present Meta - Junior Software Engineer<br></br>
+                    2014-2018 Netflix - Junior Software Engineer<br></br>
+                    2009-2014 Amazon - Junior Software Engineer<br></br>
+                    2008-2009 Apple - Junior Software Engineer<br></br>
+                    2006-2008 Microsoft - Junior Software Engineer<br></br>
+                  </p>
+                </div>
+              </div>
+
+              <div className="card">
+                <p>Available</p>
+              </div>
+            </div>
+
+            <p className="read-the-docs">This is a work in progress</p>
+          </div>
+
+          
+        <div className="containerconnections">
+          <div className="card">
+          <h2>Connections List:</h2>
+          <div className="paragraph-font">
+          <img src={pfp} className="logo-connections" alt="pfp PlaceHolder" />
+          <p>Talip Tun</p>
+          <p>Yaseen Barlas</p>
+          <p>Ahmad Sabsaby</p>
+          <p>Sayed Iqbal</p>
+          
+          </div>
+          </div>
+        </div>
 
 
-        <p>{getCountryName()}</p>
-    </div>
-
-
-
-      <div className="card">
-        <h2>Experience</h2>
-        
-        <div className='trending'>
-        <p>
-          2018-Present Meta - Junior Software Engineer<br></br>
-          2014-2018 Netflix - Junior Software Engineer<br></br>
-          2009-2014 Amazon - Junior Software Engineer<br></br>
-          2008-2009 Apple - Junior Software Engineer<br></br>
-          2006-2008 Microsoft - Junior Software Engineer<br></br>
-        </p>
         </div>
 
       </div>
-
-      <div className="card">
-        <p>Available</p>
-
-        </div>
-        </div>
-
-
-      <p className="read-the-docs">
-        This is a work in progress
-      </p>
-      </div>
-  </div>
-
-  <div className="containerconnections">
-    <h2>Connections List:</h2>
-
-
-  </div>
-</div>
-        </>
-    )
+    </>
+  );
 }
 
-export default Home
+export default Home;
