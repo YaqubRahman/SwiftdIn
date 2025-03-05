c7f3fc57cdb44b980a4f1d43b0d4d0e876cc556e
YaqubRahman <YaqubRahman999@gmail.com>
2025-03-05 00:02:32 +0000

    fix/small-fixes-and-removed-development-borders

--- yaqubs-personal-linkedin-wesbite/src/App.tsx
+++ yaqubs-personal-linkedin-wesbite/src/App.tsx
@@ -12,8 +12,8 @@ export default function App() {
     <Router>
       <Routes>
 
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<LoginSignup/>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<LoginSignup/>} />
         <Route path="/countrychoose" element={<CountryChoose/>}/>
         <Route path="/landingpage" element={<LandingPage/>}/>
 
