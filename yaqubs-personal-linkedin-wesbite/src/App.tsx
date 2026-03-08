40005e729e1b781b1a1d3a06f7c3b192290bb5bd
YaqubRahman <YaqubRahman999@gmail.com>
2026-03-08 13:04:28 +0000

    feat: landing page change

--- yaqubs-personal-linkedin-wesbite/src/App.tsx
+++ yaqubs-personal-linkedin-wesbite/src/App.tsx
@@ -1,25 +1,25 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react'
-import './App.css'
-import Home from './pages/Home';
-import { LoginSignup } from './pages/LoginSignup';
-import  CountryChoose  from './pages/CountryChoose';
-import LandingPage from './pages/LandingPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { LoginSignup } from "./pages/LoginSignup";
+import CountryChoose from "./pages/CountryChoose";
+import LandingPage from "./pages/LandingPage";
+import "tailwindcss";
+//import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default function App() {
   return (
     <>
-    <Router>
-      <Routes>
-
-        <Route path="/home" element={<Home />} />
-        <Route path="/loginsignup" element={<LoginSignup/>} />
-        <Route path="/countrychoose" element={<CountryChoose/>}/>
-        <Route path="/" element={<LandingPage/>}/>
-
-      </Routes>
-    </Router></>
-)}
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/loginsignup" element={<LoginSignup />} />
+          <Route path="/countrychoose" element={<CountryChoose />} />
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
--- yaqubs-personal-linkedin-wesbite/src/pages/LandingPage.tsx
+++ yaqubs-personal-linkedin-wesbite/src/pages/LandingPage.tsx
@@ -1,109 +1,325 @@
+"use client";
+
+import { CheckIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import logo from "../assets/SwiftdIn_Logo.png";
-import "../components/LandingPage.css";
 import { Link } from "react-router-dom";
-import office from "../assets/LandingPage/OfficePhoto.png";
-import tech from "../assets/LandingPage/TechSpeedImage.png";
-import simple from "../assets/LandingPage/SimpleBoxImage.png";
 
-function LandingPage() {
+const navigation = [
+  { name: "Home", href: "" },
+  { name: "Features", href: "#" },
+  { name: "Pricing", href: "#" },
+  { name: "Reviews", href: "#" },
+];
+
+const tiers = [
+  {
+    name: "Pleb",
+    id: "tier-hobby",
+    href: "#",
+    priceMonthly: "£0.99",
+    description: "The perfect plan if you're a pleb.",
+    features: [
+      "0.5 products",
+      "Up to 1.2 subscribers",
+      "Sub-optimal analytics",
+      "24-minute support response time",
+    ],
+    featured: false,
+  },
+  {
+    name: "Gigachad",
+    id: "tier-enterprise",
+    href: "#",
+    priceMonthly: "£1999",
+    description: "Only for the mighty",
+    features: [
+      "Gigachad products",
+      "Gigachad subscribers",
+      "Gigachad analytics",
+      "24 decade response time",
+      "Gigachad automations",
+      "Gigachad integrations",
+    ],
+    featured: true,
+  },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
-    <>
-      <h1>SwiftdIn</h1>
-      <p>Smarter, Simpler, Smoother</p>
-
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img src={logo} alt="Swiftdn Logo" width="50" height="40" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Reviews
-                </a>
-              </li>
-            </ul>
-            <div className="login-signup-button">
+    <div className="relative min-h-screen overflow-hidden bg-linear-to-b from-white via-sky-50 to-white">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav
+          aria-label="Global"
+          className="flex items-center justify-between p-6 lg:px-8"
+        >
+          <div className="flex lg:flex-1">
+            <a className="-m-1.5 p-1.5">
+              <span className="sr-only">SwiftdIn</span>
+              <img alt="" src={logo} className="h-8 w-auto" />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm/6 font-semibold text-gray-900"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
               <Link
                 to="/loginsignup"
-                style={{ textDecoration: "none", color: "inherit" }}
+                className="text-sm/6 font-semibold text-gray-900"
               >
-                <button type="button" className="btn btn-info">
-                  Login/Signup
-                </button>
+                Log in <span aria-hidden="true">&rarr;</span>
               </Link>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+        <Dialog
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+          className="lg:hidden"
+        >
+          <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img alt="" src={logo} className="h-8 w-auto" />
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <Link
+                    to="/loginsignup"
+                    className="text-sm/6 font-semibold text-gray-900"
+                  >
+                    Log in <span aria-hidden="true">&rarr;</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </header>
 
-      <div className="hero-section">
-        <div className="hero-text">
-          <h1 className="gradient-text">A new alternative to LinkedIn</h1>
-          <p>Smarter 💡, Simpler 🪶, Smoother 🌊</p>
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 
+-translate-x-1/2 rotate-30 
+bg-linear-to-tr from-[#0025ac] to-[#89edfc] 
+opacity-40 blur-3xl mix-blend-multiply"
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Announcing our next round of funding.{" "}
+              <a href="#" className="font-semibold text-indigo-600">
+                <span aria-hidden="true" className="absolute inset-0" />
+                Read more <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="text-5xl font-semibold tracking-tight sm:text-7xl bg-gradient-to-r from-emerald-500 to-blue-500 bg-clip-text text-transparent">
+              A new alternative to LinkedIn
+            </h1>
+
+            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+              <p>Smarter 💡, Simpler 🪶, Smoother 🌊</p>
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#0800ff] to-[#08ffce] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
+          />
         </div>
       </div>
-
-      <div className="card-row">
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={office} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">
-              Crafted to empower connections. Designed for a more formal,
-              professional, and effective networking experience
-            </p>
-          </div>
+      <div className="relative px-6 py-24 sm:py-32 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 
+-translate-x-1/2 
+bg-linear-to-tr from-[#0800ff] to-[#08ffce] 
+opacity-40 blur-3xl mix-blend-multiply"
+          />
         </div>
-
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={tech} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">
-              Experience the power of swift innovation. Our platform is designed
-              to keep you ahead—seamless, efficient, and built for progress
-            </p>
-          </div>
+        <div className="mx-auto max-w-4xl text-center">
+          <h1 className="text-base/7 font-semibold text-indigo-600">Pricing</h1>
+          <p className="mt-2 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl">
+            Choose the right plan for you
+          </p>
         </div>
-
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={simple} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">
-              Precision in design. Simplicity in execution. Everything you need,
-              ordered and streamlined for a clean and effortless experience
-            </p>
-          </div>
+        <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-gray-600 sm:text-xl/8">
+          Choose an affordable plan that’s packed with the best features for
+          engaging your audience, creating customer loyalty, and driving sales.
+        </p>
+        <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
+          {tiers.map((tier, tierIdx) => (
+            <div
+              key={tier.id}
+              className={classNames(
+                tier.featured
+                  ? "relative bg-gradient-to-br from-[#f7d774] via-[#d4af37] to-[#b8860b] shadow-xl shadow-yellow-900/20 ring-1 ring-[#d4af37]/40 rounded-3xl p-8 sm:p-10"
+                  : "bg-white/60 sm:mx-8 lg:mx-0",
+                tier.featured
+                  ? ""
+                  : tierIdx === 0
+                    ? "rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl"
+                    : "sm:rounded-t-none lg:rounded-tr-3xl lg:rounded-bl-none",
+                "rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10",
+              )}
+            >
+              <h3
+                id={tier.id}
+                className={classNames(
+                  tier.featured ? "text-indigo-400" : "text-indigo-600",
+                  "text-base/7 font-semibold",
+                )}
+              >
+                {tier.name}
+              </h3>
+              <p className="mt-4 flex items-baseline gap-x-2">
+                <span
+                  className={classNames(
+                    tier.featured ? "text-gray-900" : "text-gray-900",
+                    "text-5xl font-semibold tracking-tight",
+                  )}
+                >
+                  {tier.priceMonthly}
+                </span>
+                <span
+                  className={classNames(
+                    tier.featured ? "text-gray-900" : "text-gray-900",
+                    "text-base",
+                  )}
+                >
+                  /month
+                </span>
+              </p>
+              <p
+                className={classNames(
+                  tier.featured ? "text-gray-900" : "text-gray-900",
+                  "mt-6 text-base/7",
+                )}
+              >
+                {tier.description}
+              </p>
+              <ul
+                role="list"
+                className={classNames(
+                  tier.featured ? "text-gray-900" : "text-gray-900",
+                  "mt-8 space-y-3 text-sm/6 sm:mt-10",
+                )}
+              >
+                {tier.features.map((feature) => (
+                  <li key={feature} className="flex gap-x-3">
+                    <CheckIcon
+                      aria-hidden="true"
+                      className={classNames(
+                        tier.featured ? "text-indigo-400" : "text-indigo-600",
+                        "h-6 w-5 flex-none",
+                      )}
+                    />
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+              <a
+                href={tier.href}
+                aria-describedby={tier.id}
+                className={classNames(
+                  tier.featured
+                    ? "bg-indigo-500 text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-indigo-500"
+                    : "text-indigo-600 inset-ring inset-ring-indigo-200 hover:inset-ring-indigo-300 focus-visible:outline-indigo-600",
+                  "mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10",
+                )}
+              >
+                Get started today
+              </a>
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
-
-export default LandingPage;
